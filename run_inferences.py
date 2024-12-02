1ddb84b8dd98d0ce61e700aa48762fcbb1029e1a
Jingwen Zhang <jingwen_zhang@g.harvard.edu>
2024-12-01 23:29:05 -0500

    add evaluation after inferences

--- run_inferences.py
+++ run_inferences.py
@@ -51,25 +51,25 @@ for line in (open(data_dir)):
         file.write(json_str + '\n')
 
 
-    # Evaluation
-    result_path = f'{output_dir}/GSM8K_{output_name}_{timestamp_str}.jsonl'
+# Evaluation
+result_path = f'{output_dir}/GSM8K_{output_name}_{timestamp_str}.jsonl'
 
-    correct = 0
-    total = 0
-    
-    with open(f'{output_dir}/GSM8K_{output_name}_{timestamp_str}_eval.txt', 'w') as output_file:
-        for line_y, line_pred in zip(open(data_dir), open(result_path)):
-            result = json.loads(line_pred)['result']
-            true_y = json.loads(line_y)['ans']
-            pred = result.split('\n')[-1]
-            eval_result = '0'
-            if str(true_y) in pred or str(int(true_y)) in pred:
-                eval_result = '1'
-                correct += 1
-            total += 1
-            output_file.write(f"{eval_result} | {int(true_y)} | {pred}\n")
+correct = 0
+total = 0
 
-    
-    print(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
-    output_file.write(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
+with open(f'{output_dir}/GSM8K_{output_name}_{timestamp_str}_eval.txt', 'w') as output_file:
+    for line_y, line_pred in zip(open(data_dir), open(result_path)):
+        result = json.loads(line_pred)['result']
+        true_y = json.loads(line_y)['ans']
+        pred = result.split('\n')[-1]
+        eval_result = '0'
+        if str(true_y) in pred or str(int(true_y)) in pred:
+            eval_result = '1'
+            correct += 1
+        total += 1
+        output_file.write(f"{eval_result} | {int(true_y)} | {pred}\n")
+
+
+print(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
+output_file.write(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
 
