c3a5c2439285d05a5cb8dadf9f98aba265fa5618
Jingwen Zhang <jingwen_zhang@g.harvard.edu>
2024-12-03 23:22:55 -0500

    update validation to account for 1)different format; 2)mismatch in orders

--- validation.py
+++ validation.py
@@ -3,25 +3,48 @@ import json
 def check_answer(true_y, result):
     pred = result.split('\n')[-1]
     eval_result = False
-    if str(true_y) in pred or str(int(true_y)) in pred:
+    if str(true_y) in pred or str(int(true_y)) in pred or f"{int(true_y):,}" in pred:
         eval_result = True
     return eval_result
 
 def validator(data_path, result_filepath, output_filepath):
+    data_dict = {}
+    with open(data_path, 'r') as data_file:
+        for line in data_file:
+            line_y = json.loads(line)
+            question = line_y['question']
+            data_dict[question] = line_y
+
+    result_dict = {}
+    with open(result_filepath, 'r') as result_file:
+        for line in result_file:
+            line_pred = json.loads(line)
+            input_question = line_pred['input']
+            result_dict[input_question] = line_pred
+        
     correct = 0
     total = 0
     with open(output_filepath, 'w') as output_file:
-        for line_y, line_pred in zip(open(data_path), open(result_filepath)):
-            result = json.loads(line_pred)['result']
-            true_y = json.loads(line_y)['ans']
-            pred = result.split('\n')[-1]
-            eval_result = '0'
-            if str(true_y) in pred or str(int(true_y)) in pred:
-                eval_result = '1'
-                correct += 1
-            total += 1
-            output_file.write(f"{eval_result} | {int(true_y)} | {pred}\n")
+        for question, line_y in data_dict.items():
+            if question in result_dict:
+                line_pred = result_dict[question]
+                result = line_pred['result']
+                true_y = line_y['ans']
+                pred = result.split('\n')[-1]
+                eval_result = '0'
+                if str(true_y) in pred or str(int(true_y)) in pred or f"{int(true_y):,}" in pred:
+                    eval_result = '1'
+                    correct += 1
+                # else:
+                    # output_file.write(f"{eval_result} | {int(true_y)} | {pred}\n")
+                total += 1
+                output_file.write(f"{eval_result} | {int(true_y)} | {pred}\n")
 
+        print(f'#Question: {total}, #Correct: {correct}, Acc: {correct/total},')
+        output_file.write(f'#Question: {total}, #Correct: {correct}, Acc: {correct/total},')       
 
-        print(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
-        output_file.write(f'Total number:{total},Correct number:{correct},Accuracy:{correct/total}')
+if __name__ == '__main__':
+    result_filepath = 'GSM8K_qwen2_5-7b_train_distill_True.jsonl'
+    data_path = 'gsm8k/train.jsonl'
+    output_filepath = 'gsm8k_qwen2_5-7b_train_distill_True_eval.txt'
+    validator(data_path, result_filepath, output_filepath)
